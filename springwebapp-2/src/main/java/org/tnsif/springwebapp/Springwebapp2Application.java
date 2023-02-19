85ca83b7b878c75320e2c758bc33566890664c7a
Admin <Admin@BYOD-661252>
2023-02-19 18:45:09 +0530

    First commit

---
+++ springwebapp-2/src/main/java/org/tnsif/springwebapp/Springwebapp2Application.java
@@ -0,0 +1,16 @@
+package org.tnsif.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp2Application.class, args);
+		int x=15;
+		System.out.println(x);
+	}
+	}
+
+
